dc00d9868e77213b5f512225b4611d8dda9827fd
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-19 19:43:41 -0800

    Changed func name

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,13 +6,13 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 18:15:38 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/19 19:07:39 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/19 19:30:49 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strstr(char *haystack, char *needle, size_t len)
+char	*ft_strnstr(char *haystack, char *needle, size_t len)
 {
 	size_t i;
 	size_t j;
