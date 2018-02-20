7bc49fc5869e51094d7e3af8a3a2856684df3f74
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-20 11:02:14 -0800

    added strcpy and strncpy

---
+++ srcs/ft_strncpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/20 10:45:03 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/02/20 10:52:13 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	size_t	i;
+
+	i = -1;
+	while (src[++i] && i < len)
+		dst[i] = src[i];
+	while (i < len)
+		dst[i++] = '\0';
+	return (dst);
+}
