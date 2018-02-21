c930d836d77a16add550a0b8ac0d813589acb565
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-20 16:29:05 -0800

    updated header

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 14:25:47 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/20 12:38:46 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/20 16:23:36 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <string.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <unistd.h>
 
 int		ft_atoi(char *s);
 void	ft_bzero(void *s, size_t n);
@@ -24,12 +25,17 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+char	*ft_itoa(int n);
 void	*ft_memccpy(void *dst, void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char const *s, int fd);
 char	*ft_strcat(char *restrict s1, const char *restrict s2);
 char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 char	*ft_strchr(const char *s, int c);
@@ -42,10 +48,12 @@ char	*ft_strncpy(char *dst, const char *src, size_t len);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(char *haystack, char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strrev(char *str);
 char	*ft_strstr(char *haystack, char *needle);
 int		ft_toupper(int a);
 int		ft_tolower(int a);
 
 # define ISSPACE(c) (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v' || c == '\f')
+# define ABS(x) (((x) < 0) ? -(x) : (x))
 
 #endif
