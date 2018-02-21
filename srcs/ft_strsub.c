ef58ae48f76b5bf7e0447165aa3e4b8bb9d4db1f
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-20 18:26:18 -0800

    added strub

---
+++ srcs/ft_strsub.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/20 18:15:43 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/02/20 18:26:01 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+
+	if (!s || !(res = ft_strnew(len)))
+		return (NULL);
+	return (ft_strncpy(res, s + start, len));
+}
