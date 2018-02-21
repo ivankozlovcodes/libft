0417eb6c49c97e39f8b3255144990cca3a2c0484
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-21 08:39:03 -0800

    updated includes

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 14:25:47 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/20 16:35:36 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/21 08:36:47 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,30 +31,49 @@ void	*ft_memccpy(void *dst, void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, void *src, size_t n);
+void	ft_memdel(void **ap);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putchar(char c);
 void	ft_putendl_fd(char const *s, int fd);
+void	ft_putendl(char const *s);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr(int n);
 void	ft_putstr_fd(char const *s, int fd);
+void	ft_putstr(char const *s);
 char	*ft_strcat(char *restrict s1, const char *restrict s2);
 char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 char	*ft_strchr(const char *s, int c);
-int		ft_strcmp(char *s1, char *s2);
+void	ft_strclr(char *s);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
+void	ft_strdel(char **as);
 char	*ft_strdup(const char *s1);
+int		ft_strequ(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_striter(char *s, void (*f)(char *));
 size_t	ft_strlen(const char *s);
-int		ft_strncmp(char *s1, char *s2, size_t n);
+char	*ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strnew(size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(char *haystack, char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strrev(char *str);
+char	**ft_strsplit(char const *s, char c);
 char	*ft_strstr(char *haystack, char *needle);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s);
 int		ft_toupper(int a);
 int		ft_tolower(int a);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
-# define ISSPACE(c) (c == ' ' || c == '\t' || c == '\n' || ISSPACE2(c))
+# define ISSPACE(c) (ISSPACE1(c) || ISSPACE2(c))
+# define ISSPACE1(c) (c == ' ' || c == '\t' || c == '\n')
 # define ISSPACE2(c) (c == '\r' || c == '\v' || c == '\f')
 # define ABS(x) (((x) < 0) ? -(x) : (x))
 
