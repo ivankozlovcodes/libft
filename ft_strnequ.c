6e93f2006d3fe1b9a3d0e3c72ddd5de3554b9ed8
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-22 09:27:27 -0800

    add check for null for s2 in order to fix segfault

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 18:06:06 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/20 18:12:29 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/22 09:26:09 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (s1 && s1)
+	if (s1 && s2)
 		return (ft_strncmp(s1, s2, n) == 0 ? 1 : 0);
 	return (0);
 }
