d82ebbb66bd9cf19a4128d1048e2c933cf1ec5d8
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-21 18:07:34 -0800

    fixed norm

--- srcs/ft_strncat.c
+++ srcs/ft_strncat.c
@@ -6,13 +6,13 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 20:17:10 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/20 10:11:38 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/21 18:07:02 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
 	char	*head;
 
