976bc119ca4d97777f6f00e84986e36b2c7363a7
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-22 09:26:57 -0800

    removed old func commented

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,23 +6,11 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 11:24:08 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/22 09:17:12 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/22 09:20:09 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// void	*ft_memchr(const void *s, int c, size_t n)
-// {
-// 	const unsigned char	*p;
-// 	unsigned char		b;
-
-// 	p = (const unsigned char *)s;
-// 	b = (unsigned char)c;
-// 	while (*p != b && n-- && *p)
-// 		p++;
-// 	return (*p == b ? (void *)p : NULL);
-// }
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
