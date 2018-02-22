649e31e01a81e540b2e43e30c7ed44e1c960061e
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-21 21:11:44 -0800

    fixed tests for basic input

--- srcs/ft_lstmap.c
+++ srcs/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 17:34:50 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/21 17:50:10 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/21 21:11:16 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	lst = lst->next;
 	while (lst)
 	{
+		map->next = f(lst);
 		map = map->next;
-		map = f(lst);
 		lst = lst->next;
 	}
 	return (head);
