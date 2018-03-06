a78877b2eac16713e3f56f398385274b8057909f
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-05 20:18:31 -0800

    Fixed error with int and uint comparing

--- srcs/math/ft_pow.c
+++ srcs/math/ft_pow.c
@@ -6,13 +6,13 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 19:06:29 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/05 19:15:19 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/05 20:17:41 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 long long int	ft_pow(int n, unsigned int p)
 {
-	int				i;
+	unsigned int	i;
 	long long int	res;
 
 	if (p == 0)
