29e9ddd04e309ece0e23c498008794185ad68f24
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-08 19:49:25 -0800

    Fixed lliitoa for negative numbers

--- srcs/numbers/ft_llitoa_tobase.c
+++ srcs/numbers/ft_llitoa_tobase.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 18:56:20 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/08 11:01:35 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/08 19:47:40 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ char	*ft_llitoa_tobase(long long int n, char *base)
 	while (nbr /= base_len)
 		len++;
 	nbr = (unsigned long long int)n;
+	nbr = n < 0 ? -nbr : nbr;
 	len += n < 0;
 	if (!(str = (char *)malloc(sizeof(char) * len)))
 		return (NULL);
 	str[--len] = '\0';
-	while (len-- >= 0)
+	while (len-- > 0)
 	{
 		str[len] = base[nbr % base_len];
 		nbr /= base_len;
 	}
-	if (n < 0)
-		str[0] = '-';
+	str[0] = n < 0 ? '-' : str[0];
 	return (str);
 }
