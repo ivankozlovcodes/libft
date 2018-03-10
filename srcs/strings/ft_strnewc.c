ac4913cd7de406e3a224857a59d315b89545f583
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-10 12:33:30 -0800

    Revert "Fixed segfault in case size is less than 0"
    This reverts commit 803caa4d1dc843b1c34e0a27e84a996af350884d.

--- srcs/strings/ft_strnewc.c
+++ srcs/strings/ft_strnewc.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/09 21:11:57 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/10 12:15:59 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/09 21:21:22 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@ char	*ft_strnewc(size_t size, char c)
 {
 	char	*s;
 
-	if (size < 0)
-		return (NULL);
 	s = (char *)malloc(sizeof(char) * (size + 1));
 	s[size] = '\0';
 	while (size-- != 0)
