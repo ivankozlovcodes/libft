1494a705bd21074aea71fa260cd9b93cd982a90d
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-12 16:14:26 -0700

    ft_putnstr: fwreeing poiner after printing

--- srcs/chars/ft_putnchar.c
+++ srcs/chars/ft_putnchar.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 15:16:30 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/12 15:16:54 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/12 16:02:53 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 void	ft_putnchar(char c, int n)
 {
+	char	*s;
+
 	if (n > 0)
-		ft_putstr(ft_strnewc(n, c));
+	{
+		s = ft_strnewc(n, c);
+		ft_putstr(s);
+		free(s);
+	}
 }
