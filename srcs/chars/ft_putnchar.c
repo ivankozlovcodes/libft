d9e4d9219ba128499ca62f5e9719dad47f7aaca4
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-12 15:18:38 -0700

    Added putnchar

--- srcs/chars/ft_putnchar.c
+++ srcs/chars/ft_putnchar.c
@@ -1,21 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   chars.h                                            :+:      :+:    :+:   */
+/*   ft_putnchar.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/05 15:00:57 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/12 15:17:43 by ikozlov          ###   ########.fr       */
+/*   Created: 2018/03/12 15:16:30 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/03/12 15:16:54 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_CHARS_H
-# define FT_CHARS_H
+#include "libft.h"
 
-# include <unistd.h>
-
-void	ft_putnstr(const char *s, size_t size);
-void	ft_putnchar(char c, int n);
-
-#endif
+void	ft_putnchar(char c, int n)
+{
+	if (n > 0)
+		ft_putstr(ft_strnewc(n, c));
+}
