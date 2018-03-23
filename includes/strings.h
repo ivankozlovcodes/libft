c437bdd20a1cfc523f8fb01609d23a7ae832e02b
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-22 21:35:18 -0700

    Added consts for space and new line

--- includes/strings.h
+++ includes/strings.h
@@ -6,14 +6,16 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 10:16:33 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/10 20:08:36 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/22 21:30:06 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STRINGS_H
 # define FT_STRINGS_H
-
 # include <stddef.h>
+
+# define SPACE " "
+# define NEW_LINE "\n"
 
 char	*ft_strstrend(char *haystack, char *needle);
 int		ft_strchri(char *s, char c);
