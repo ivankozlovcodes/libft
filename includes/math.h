b61ed26a365b8136fc51147e670cc443cfd91577
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-28 14:06:12 -0700

    Added struct t_point

--- includes/math.h
+++ includes/math.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 15:08:37 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/27 21:03:38 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/28 14:05:48 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,18 @@
 # define MAX(a, b) ((a) > (b) ? (a) : (b))
 # define DISTANCE(a,b,c,d) (ft_sqrt(ft_pow(c - a, 2) + ft_pow(d - b, 2)))
 # define ABS(x) (((x) < 0) ? -(x) : (x))
+# define SETX(p, _x) ((p.x = _x))
+# define SETY(p, _y) ((p.y = _y))
+# define SETXY(p, _x, _y) ((SETX(p, _x) && SETY(p, _y)))
 
-long long int	ft_pow(int n, unsigned int p);
-int				ft_sqrt(int nb);
+struct					s_point
+{
+	int					x;
+	int					y;
+};
+typedef struct s_point	t_point;
+
+long long int			ft_pow(int n, unsigned int p);
+int						ft_sqrt(int nb);
 
 #endif
