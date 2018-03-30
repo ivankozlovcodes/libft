44a1cc8670fa8a92b55ad800428dadc02b5dac7b
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-29 23:28:33 -0700

    Fixed leaks in ft_lstaddlst

--- srcs/list/ft_lstaddlst.c
+++ srcs/list/ft_lstaddlst.c
@@ -6,19 +6,33 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/29 20:26:23 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/29 20:57:53 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/29 23:05:40 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 
+/*
+** Argument:	1. Double pointer to destination list(a)
+**				2. Pointer to source list(b).
+** Operation:	Adds all items of list b to list a.
+**				And frees the b list after.
+*/
+
 void	ft_lstaddlst(t_list **al, t_list *bl)
 {
+	t_list	*tmp;
+
 	if (!al)
 		return ;
-	while (bl)
+	if (bl)
 	{
-		ft_lstadd(al, ft_lstnew(bl->content, bl->content_size));
-		bl = bl->next;
+		while (bl)
+		{
+			ft_lstadd(al, ft_lstnew(bl->content, bl->content_size));
+			tmp = bl->next;
+			ft_lstdelone(&bl, ft_free_content);
+			bl = tmp;
+		}
 	}
 }
