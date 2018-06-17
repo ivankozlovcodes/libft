78ba59f676fb7da9e0940ebcf846bdcdd072f3a8
Ivan Kozlov <ikozlov9000@gmail.com>
2018-06-17 00:07:07 -0700

    Added realloc to header

--- includes/memory.h
+++ includes/memory.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/27 20:40:51 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/27 20:47:28 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/06/16 23:46:12 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void				ft_memdel(void **ap);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memset(void *b, int c, size_t len);
 int					ft_free(int count, ...);
+void				*ft_realloc(void *ptr, size_t size, size_t newsize);
 
 #endif
