374d0b82c7535a59c6abcb26724b0c3cd4c9cb9b
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-26 19:23:47 -0700

    Added string_appendn function

--- includes/dstring.h
+++ includes/dstring.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dstring.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/16 23:30:09 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/06/17 00:05:59 by ikozlov          ###   ########.fr       */
+/*   Updated: 2019/05/26 18:11:02 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ typedef struct s_string		t_string;
 
 t_string					*string_init(size_t capacity);
 t_bool						string_append(struct s_string *s, char *add);
+t_bool						string_appendn(struct s_string *s, char *add, size_t n);
 char						*string_destroy(t_string *s, t_bool save_content);
 
 #endif
--- srcs/dstring/string_append.c
+++ srcs/dstring/string_append.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   string_append.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/16 23:39:06 by ikozlov           #+#    #+#             */
-/*   Updated: 2019/03/05 03:32:07 by ikozlov          ###   ########.fr       */
+/*   Updated: 2019/05/26 18:10:42 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 #include "ftstring.h"
 #include "memory.h"
 #include "dstring.h"
+
+t_bool		string_appendn(struct s_string *s, char *add, size_t n)
+{
+	char	*tmp;
+	t_bool	ret;
+
+	tmp = ft_strnew(n);
+	ft_strncpy(tmp, add, n);
+	ret = string_append(s, tmp);
+	free(tmp);
+	return (ret);
+}
 
 t_bool		string_append(struct s_string *s, char *add)
 {
