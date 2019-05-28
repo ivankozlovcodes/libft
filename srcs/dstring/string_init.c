637c36cba8c83b8ee2e5da6b7577c5e29e5881fb
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-28 12:57:15 -0700

    Add bzero for when creating string

--- srcs/dstring/string_init.c
+++ srcs/dstring/string_init.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   string_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/16 23:33:51 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/06/16 23:39:04 by ikozlov          ###   ########.fr       */
+/*   Updated: 2019/05/28 12:56:48 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "memory.h"
 #include "dstring.h"
 
 t_string	*string_init(size_t capacity)
@@ -19,9 +19,9 @@ t_string	*string_init(size_t capacity)
 
 	if (capacity <= 0)
 		capacity = DEFAULT_CAPACITY;
-	s = malloc(sizeof(t_string));
+	s = ft_memalloc(sizeof(t_string));
 	s->capacity = capacity;
-	s->content = malloc(sizeof(char) * s->capacity);
+	s->content = ft_memalloc(sizeof(char) * s->capacity);
 	s->length = 0;
 	return (s);
 }
