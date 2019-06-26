9162e7d667b58a37bb61918d2358615df7ca0859
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-06-26 02:49:12 -0700

    Added reverse color

--- includes/colors.h
+++ includes/colors.h
@@ -3,23 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   colors.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 17:16:16 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/10 17:03:14 by ikozlov          ###   ########.fr       */
+/*   Updated: 2019/06/25 23:25:40 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_H
 # define COLORS_H
 
-# define RED   "\x1B[31m"
-# define GRN   "\x1B[32m"
-# define YEL   "\x1B[33m"
-# define BLU   "\x1B[34m"
-# define MAG   "\x1B[35m"
-# define CYN   "\x1B[36m"
-# define WHT   "\x1B[37m"
-# define RESET "\x1B[0m"
+# define RED    "\x1B[31m"
+# define GRN    "\x1B[32m"
+# define YEL    "\x1B[33m"
+# define BLU    "\x1B[34m"
+# define MAG    "\x1B[35m"
+# define CYN    "\x1B[36m"
+# define WHT    "\x1B[37m"
+# define RESET  "\x1B[0m"
+
+# define REVERSE "\033[7m"
+# define UNDLINE "\033[4m"
 
 #endif
