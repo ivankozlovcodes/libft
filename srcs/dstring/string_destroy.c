3e3a30452d34e3a6bd14b7deab65df68ee0602f2
Ivan Kozlov <ikozlov@student.42.us.org>
2019-09-01 09:41:18 -0700

    Add dstring string destroy content method

--- srcs/dstring/string_destroy.c
+++ srcs/dstring/string_destroy.c
@@ -3,20 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   string_destroy.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/16 23:51:04 by ikozlov           #+#    #+#             */
-/*   Updated: 2019/07/10 08:37:50 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/08/31 10:59:44 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dstring.h"
 #include <stdlib.h>
 
+void	string_destroy_content(t_string *s)
+{
+	free(s->content);
+	s->capacity = 0;
+	s->length = 0;
+}
+
 char	*string_destroy(t_string *s, bool save_content)
 {
 	char	*tmp;
 
+	if (!s)
+		return (NULL);
 	tmp = s->content;
 	if (save_content == false)
 	{
